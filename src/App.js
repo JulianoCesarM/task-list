182a4e4bc6b9fb76ad717248f7a0dfac3a8b820d
Juliano Cesar Magoga <julianoc.m@hotmail.com>
2023-11-29 19:31:49 -0300

    My first component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import React from 'react';
-import './App.css';
+import React from "react"
+import "./App.css"
+import Main from "./components/Main"
 
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello, World!</h1>
-    </div>
-  );
+  return <Main />
 }
