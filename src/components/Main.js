f674373f8c15d11080ddde8b4673ec28f22ac898
Juliano Cesar Magoga <julianoc.m@hotmail.com>
2023-12-04 16:35:37 -0300

    creating functionality to add tasks

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,9 +4,19 @@ import { FaEdit, FaWindowClose } from "react-icons/fa"
 export default class Main extends Component {
   state = {
     novaTarefa: "",
-    tarefas: ["Fazer café", "Arrumar a cama", "Estudar"],
+    tarefas: [],
   }
-
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const { tarefas } = this.state
+    let { novaTarefa } = this.state
+    novaTarefa = novaTarefa.trim()
+    if (tarefas.indexOf(novaTarefa) !== -1) return
+    const novasTarefas = [...tarefas]
+    this.setState({
+      tarefas: [...novasTarefas, novaTarefa],
+    })
+  }
   handleChange = (e) => {
     this.setState({
       novaTarefa: e.target.value,
@@ -17,7 +27,7 @@ export default class Main extends Component {
     return (
       <div className="main">
         <h1>Lista de tarefas</h1>
-        <form action="#" className="form">
+        <form action="#" className="form" onSubmit={this.handleSubmit}>
           <input type="text" onChange={this.handleChange} value={novaTarefa} />
           <button type="submit">Adicionar</button>
         </form>
