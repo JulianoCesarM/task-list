ce549b0a91a4fb7cc40057883d4a588870b9d89e
Juliano Cesar Magoga <julianoc.m@hotmail.com>
2023-12-04 17:01:11 -0300

    creating functionality to edit tasks

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,25 +5,42 @@ export default class Main extends Component {
   state = {
     novaTarefa: "",
     tarefas: [],
+    index: -1,
   }
   handleSubmit = (e) => {
     e.preventDefault()
-    const { tarefas } = this.state
+    const { tarefas, index } = this.state
     let { novaTarefa } = this.state
     novaTarefa = novaTarefa.trim()
     if (tarefas.indexOf(novaTarefa) !== -1) return
     const novasTarefas = [...tarefas]
-    this.setState({
-      tarefas: [...novasTarefas, novaTarefa],
-      novaTarefa: "",
-    })
+
+    if (index === -1) {
+      this.setState({
+        tarefas: [...novasTarefas, novaTarefa],
+        novaTarefa: "",
+      })
+    } else {
+      novasTarefas[index] = novaTarefa
+      this.setState({
+        tarefas: [...novasTarefas],
+        index: -1,
+      })
+    }
   }
+
   handleChange = (e) => {
     this.setState({
       novaTarefa: e.target.value,
     })
   }
-  handleEdit = (e, index) => {}
+
+  handleEdit = (e, index) => {
+    const { tarefas } = this.state
+
+    this.setState({ index, novaTarefa: tarefas[index] })
+  }
+
   handleDelete = (e, index) => {
     const { tarefas } = this.state
     let novasTarefas = [...tarefas]
