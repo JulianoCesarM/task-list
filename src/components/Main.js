0f14fedf230129e8748af9e0078b049efdfe514b
Juliano Cesar Magoga <julianoc.m@hotmail.com>
2023-12-04 16:08:54 -0300

    rendering tasks on screen and adding import icons

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react"
 import "./Main.css"
-
+import { FaEdit, FaWindowClose } from "react-icons/fa"
 export default class Main extends Component {
   state = {
     novaTarefa: "",
+    tarefas: ["Fazer café", "Arrumar a cama", "Estudar"],
   }
 
   handleChange = (e) => {
@@ -12,15 +13,19 @@ export default class Main extends Component {
     })
   }
   render() {
-    const { novaTarefa } = this.state
+    const { novaTarefa, tarefas } = this.state
     return (
       <div className="main">
         <h1>Lista de tarefas</h1>
-        <form action="#">
-          <input type="text" onChange={this.handleChange} />
-          <button type="submit">Enviar</button>
+        <form action="#" className="form">
+          <input type="text" onChange={this.handleChange} value={novaTarefa} />
+          <button type="submit">Adicionar</button>
         </form>
-        <p>{novaTarefa}</p>
+        <ul>
+          {tarefas.map((tarefa) => (
+            <li key={tarefa}>{tarefa}</li>
+          ))}
+        </ul>
       </div>
     )
   }
